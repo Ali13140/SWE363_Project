6ee800b5fcfebdbb3a97a40c981f7f72a17edad4
Ali <s202013140@kfupm>
2024-04-15 10:43:04 +0300

    Navigate through HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import NavBar from '../Components/NavBar';
+import { Link, useNavigate  } from 'react-router-dom';
+
 
 const HomePage = () => {
+  const nav=useNavigate();
+
+  const handleClick=(event) =>{
+    const button=event.target;
+    const buttonName=button.name;
+    if(buttonName=="list")
+      nav("/MyListPage")
+    else
+     nav("/NewListPage")
+  }
   return (
     <div> <NavBar></NavBar>
     <div className="container text-center mt-5">
       <h2>What do you want to do in the future?</h2>
       <br />
       <div className="d-grid gap-2 d-md-flex justify-content-md-center  d-md-block">
-  <button className="btn btn-primary me-md-2" type="button">Button</button>
-  <button className="btn btn-primary" type="button">Button</button>
+  <button className="btn btn-primary me-md-2" type="button" name="list" onClick={handleClick}>My List</button>
+  <button className="btn btn-primary" type="button" name='newList' onClick={handleClick}>Create New List</button>
 </div>
             
         </div>
